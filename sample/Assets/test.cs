e892bbbe5c3de6b87629e194a465cb68230665e6
Unknown <g1727035@st.t-kougei.ac.jp>
2018-05-02 18:08:46 +0900

    最初のコミット

---
+++ sample/Assets/test.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int age;
+
+            age = 30;
+
+        Debug.Log("Hello World");
+        Debug.Log(age);
+
+        int answer;
+
+        answer = 1 + 2;
+        Debug.Log(answer);
+
+        answer = 1 - 2;
+        Debug.Log(answer);
+
+        answer = 2 * 4;
+        Debug.Log(answer);
+
+        answer = 8 / 2;
+        Debug.Log(answer);
+
+        string name;
+
+        name = "sugiyama";
+
+        Debug.Log(name);
+
+        int n1 = 8;
+        int n2 = 8;
+
+        answer = n1 + n2;
+
+        Debug.Log(answer);
+
+        int herbNum = 1;
+        if (herbNum == 1) ;
+        Debug.Log("HPが５０回復");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
